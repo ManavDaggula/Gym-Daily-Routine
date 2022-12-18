f5ad362347e4dced16ce518d87e2fa8c0cf2abc3
Mihir Manoj Lakhamje <93497281+MihirLakhamje@users.noreply.github.com>
2022-12-18 14:36:51 +0530

    Revert "Profile"

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,10 @@
-
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap/dist/js/bootstrap.js';
-import Landing from './components/Landing';
-import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
-import Login from './components/Login';
-import Navbar from './components/Navbar';
-import Signup from './components/Signup';
 import './App.css';
-import Profile from './components/Profile';
+import Navbar from './components/Navbar';
+
 function App() {
 	return (
 		<>
-		<Router>
-			<Navbar/>
-			<Routes>
-				<Route exact path='/' element={<Landing/>}/>
-				{/* <Route exact path='/about' element={<About/>}/> */}
-				<Route exact path='/login' element={<Login/>}/>
-				<Route exact path='/signup' element={<Signup/>}/>
-				<Route exact path='/profile' element={<Profile/>}/>
-			</Routes>
-		</Router>
+		<Navbar/>
 		</>
 	);
 }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,9 +4,6 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-
-// import './style.scss';
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
