f09ecb836ba59f347db6d559bcd577896ff5ade6
MihirLakhamje <lakhamjemihir@gmail.com>
2022-12-18 11:59:57 +0530

    embed profile icon

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import hamburgerbtn from '../images/menuBtn.svg'
+import profileImg from '../images/profileImg.svg'
 
 export default function Navbar(props) {
     return (
@@ -8,10 +10,11 @@ export default function Navbar(props) {
                 <div class="container-fluid">
                     <div>
                         <button class="btn btn-light btn-sm" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
-                            <img src={hamburgerbtn} alt="" />
+                            <img src={hamburgerbtn} alt="toggle dowm menu" />
                         </button>
                         <span class="text mx-4">{props.currentPage} hello world</span>
                     </div>
+                    <Link to="/profile"><img src={profileImg} alt="My profile" /></Link>
                     <div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
                         <div class="offcanvas-header">
                             <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Menu</h5>
